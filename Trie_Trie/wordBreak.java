1dfa82a5ebcbbce13d6eb57c9e0eebea13a9898d
Sonu Singh <153010657+sonusingh7667@users.noreply.github.com>
2025-02-15 22:25:49 +0530

    Add files via upload

---
+++ Trie_Trie/wordBreak.java
@@ -0,0 +1,10 @@
+package Tries_Tries;
+
+public class wordBreak {
+    public static void ain(String[]args){
+        String arr[] = {"i","like","sam","samsung","mobile","ice"};
+        for (int i=0; i<arr.length; i++){
+
+        }
+    }
+}
