d59f0c2e4cc6f971a408fc25cf74886a84cc05c5
Sonu Singh <153010657+sonusingh7667@users.noreply.github.com>
2026-01-26 13:56:42 +0530

    Add files via upload

---
+++ 2D_Arrays/Buy_Sell_stock.java
@@ -0,0 +1,26 @@
+package Arrays_2D;
+
+public class Buy_Sell_stock {
+
+    public static int buyAndSellStock(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < buyPrice){
+                buyPrice = prices[i];
+            }
+            else {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args){
+        int prices[] = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(buyAndSellStock(prices));
+    }
+}
