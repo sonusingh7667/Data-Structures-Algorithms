8f2bc86cd17408a3ee5e3794c2ba51a8ee9afda5
Sonu Singh <153010657+sonusingh7667@users.noreply.github.com>
2026-02-07 19:52:02 +0530

    Refactor Sudoku solver implementation

--- BackTracking/Sudoku_Sudoku.java
+++ BackTracking/Sudoku_Sudoku.java
@@ -94,3 +94,74 @@ public class Sudoku_Sudoku {
         }
     }
 }
+
+
+
+
+
+
+//////  LeetCode Solution for sudoku game
+
+public class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean isSafe(char[][] board, int row, int col, char c) {
+
+        //row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) {
+                return false;
+            }
+        }
+        //column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c) {
+                return false;
+            }
+        }
+        //grid
+        int subRow = (row / 3) * 3;
+        int subCol = (col / 3) * 3;
+        for (int i = subRow; i < subRow + 3; i++) {
+            for (int j = subCol; j < subCol + 3; j++) {
+                if (board[i][j] == c) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+    public boolean solve(char[][] board) {
+      
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+               
+                if (board[row][col] == '.') {
+                   
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isSafe(board, row, col, c)) {
+                            board[row][col] = c; 
+
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
