226534ae0af954d4300e1977f9f1f76a05f75ac7
Sonu Singh <153010657+sonusingh7667@users.noreply.github.com>
2026-02-09 18:26:00 +0530

    Update SortAnArrayList.java

--- ArrayList_ArrayList/SortAnArrayList.java
+++ ArrayList_ArrayList/SortAnArrayList.java
@@ -1,1 +1,40 @@
 
+package ArrayList_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Swap_indexValue {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(81);
+        list.add(29);
+        list.add(31);
+        list.add(43);
+        list.add(59);
+        list.add(63);
+
+        int indx1= 1, indx2=4;
+        System.out.println(list);
+        Swap(list, indx1, indx2);
+        System.out.println(list);
+
+
+
+
+//        .................................................................................................
+
+        //Asccending Order
+//        System.out.println(list);
+//        Collections.sort(list);
+//        System.out.println(list);
+
+        //Deccending Order
+
+//        Collections.sort(list, Collections.reverseOrder());
+//        System.out.println(list);
+
+
+    }
+
+}
